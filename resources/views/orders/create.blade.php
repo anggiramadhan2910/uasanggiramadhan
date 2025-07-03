50becff69b8aee4d2cf937f23bbabbe5f768b588
anggiramadhan2910 <anggiramadhan2910@gmail.com>
2025-07-03 11:03:31 +0700

    validasi

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -3,6 +3,15 @@
 @section('content')
 <div class="container">
     <h1>Tambah Order</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul class="mb-0">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="{{ route('orders.store') }}" method="POST">
         @csrf
         <div class="mb-3">
